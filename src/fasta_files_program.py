4d13dc5905e62183d8bbd4215dd70439d2c9ed45
Brenda <brenda.titanium@hotmail.com>
2022-03-31 21:32:10 -0600

    Insertar un salto de línea en la posición 2 de cada lista

--- src/fasta_files_program.py
+++ src/fasta_files_program.py
@@ -1,8 +1,8 @@
 with open ('data/dna_sequences.txt','r') as archivo:
-    secuencias= [line.split('\t') for line in archivo]
-
+    secuencias= [line.split('\t') for line in archivo]    
 for secuencia in secuencias:
     secuencia.insert(0,'>')
+    secuencia.insert(2,'\n')
 print(secuencias) 
 
 my_file= open('data/secuencias.fasta','w')
