286c65d1196e427cbac27ac2ced43cda8eab7612
Brenda <brenda.titanium@hotmail.com>
2022-03-31 20:35:19 -0600

    Abrir el archivo y guardar su contenido en la variable: secuencias

--- src/fasta_files_program.py
+++ src/fasta_files_program.py
@@ -1,3 +1,6 @@
 with open ('data/dna_sequences.txt','r') as archivo:
     secuencias= [line.split('\t') for line in archivo]
+for i in range (0, len(secuencias)):
+    secuencias[i][0] = '>'
+    secuencias[i].append('\n')
 print(secuencias)
