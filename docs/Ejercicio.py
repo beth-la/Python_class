422bfbb79d19162abca88bf545a3ec441736161c
Brenda <brenda.titanium@hotmail.com>
2022-03-31 19:05:30 -0600

    Creamos el archivo

---
+++ docs/Ejercicio.py
@@ -0,0 +1,7 @@
+# Programa que cuenta la aparición de cada nucleotido.
+ADN= "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"
+print("La secuencia tiene la cantidad de nucleotidos: ")
+print("A=", ADN.count("A"))
+print("C=", ADN.count("C"))
+print("G=", ADN.count("G"))
+print("T=", ADN.count("T"))
