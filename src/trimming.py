4ed8f7d9c99f4c8221a8596cf7c2ab59dea61346
Brenda <brenda.titanium@hotmail.com>
2022-03-31 20:10:33 -0600

    asignandole los caracteres determinados

--- src/trimming.py
+++ src/trimming.py
@@ -1,3 +1,9 @@
 with open('data/4_input_adapters.txt','r') as archivo:
     secuencias= [line.split("\n") for line in archivo]
-    print(secuencias[0][0:14])
+    sin_adaptadores= []
+for i in range(0,len(secuencias)):
+    sin_adaptadores.append(secuencias[i][0][14:])
+print(sin_adaptadores)
+
+my_file= open('data/Sec_sin_adaptadores.txt','w')
+my_file.write('\n'.join(sin_adaptadores))
