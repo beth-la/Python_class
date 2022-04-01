0fc62662009aa8665b79ee7d4dcb55d9b8113053
Brenda <brenda.titanium@hotmail.com>
2022-03-31 21:29:59 -0600

    Escribiendo en el archivo con el formato determinado para un archivo fasta

--- src/fasta_files_program.py
+++ src/fasta_files_program.py
@@ -1,8 +1,11 @@
 with open ('data/dna_sequences.txt','r') as archivo:
     secuencias= [line.split('\t') for line in archivo]
+
 for secuencia in secuencias:
     secuencia.insert(0,'>')
-    
-#print(secuencias)
+print(secuencias) 
+
 my_file= open('data/secuencias.fasta','w')
-my_file.write('\n'.join(secuencias))
+for secuencia in secuencias:
+    my_file.write(' '.join(secuencia))
+my_file.close()
