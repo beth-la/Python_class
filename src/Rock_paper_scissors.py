dccabcc3bae0ee0678cd67181b5139e69f8c0fbf
Brenda <brenda.titanium@hotmail.com>
2022-04-09 11:08:36 -0500

    Encabezado y algoritmo de solucion

---
+++ src/Rock_paper_scissors.py
@@ -0,0 +1,46 @@
+''' Name 
+    Rock paper scissors.
+    
+Version
+    1.0
+    
+Author 
+    Lopez A. Brenda E.
+    
+Descripcion
+    Programa que simula un juego de piedra papel o tijera.
+    
+Usage
+    El programa nos permite obtener las secuencias de ADN eliminando las secuencias 
+    que corresponden a adaptadores
+
+Category
+    Recreational
+    
+Arguments
+    None
+    
+See also
+    None
+    
+'''
+# Pedir al usuario que ingrese alguna opcion 
+# Elegir la opcion de la computadora 
+
+# Si el usuario elige tijeras
+
+#   Si la computadora elije tijeras 
+#   Si la computadora elije papel
+#   Si la computadora elije roca  
+
+# Si el usuario elije papel 
+
+#   Si la computadora elije tijeras 
+#   Si la computadora elije papel
+#   Si la computadora elije roca
+
+# Si el usuario elije piedra  
+
+#   Si la computadora elije tijeras 
+#   Si la computadora elije papel
+#   Si la computadora elije roca
