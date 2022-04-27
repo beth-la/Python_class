e042eb6fea82ddb98eab1c9766a1ffaa25617763
Brenda <brenda.titanium@hotmail.com>
2022-04-26 21:29:04 -0500

    Agregando estructura de try-except

--- src/remove_adapters.py
+++ src/remove_adapters.py
@@ -28,32 +28,29 @@ See also
 # Abrir el archivo que contiene las secuencias deseadas
 # Asignar las secuencias a una lista
 
-with open('data/4_input_adapters.txt','r') as archivo:
-    secuencias= [line.split("\n") for line in archivo]
-    sin_adaptadores= []
+# Agregamos estructura try-except para avisar al usuario si es que su archivo no se encuentra.
+
+try:  
+    with open('data/4_input_adapters.txt','r') as archivo:
+        secuencias= [line.split("\n") for line in archivo]
+        sin_adaptadores= []
+        
+except IOError as io_error: 
+    print(f"El archivo {io_error.filename} no se encuentra. \n")
     
 # Excluimos la parte de la secuencia que contiene a los adaptadores
 # Estas nuevas secuencias las almacenamos en una lista nueva 
 
-for i in range(0,len(secuencias)):
-    sin_adaptadores.append(secuencias[i][0][14:])
+else:
+    
+    for i in range(0,len(secuencias)):
+        sin_adaptadores.append(secuencias[i][0][14:])
 
 # Abrimos un nuevo archivo que sera el output 
 # Ecribimos la lista con las secuencias en el archivo 
 
-my_file= open('data/Sec_sin_adaptadores.txt','w')
-my_file.write('\n'.join(sin_adaptadores))
-my_file.close()
+    my_file= open('data/Sec_sin_adaptadores.txt','w')
+    my_file.write('\n'.join(sin_adaptadores))
+    my_file.close()
 
-# Estructura Try y except del codigo:
 
-try:
-    archivo = open('data/4_input_adaptersk.txt')
-    print('file contents '+ archivo.read())
-except IOError as ex:
-    print("No se pudo encontrar el archivo: " + ex.strerror)
-finally:
-# Cerrar archivo
-    archivo.close()
-
-    
