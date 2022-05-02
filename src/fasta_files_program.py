bc6104119da80af9f099f5c3d9321acc619154d9
Brenda <brenda.titanium@hotmail.com>
2022-05-02 18:37:35 -0500

    mejorando la eficiencia del programa

--- src/fasta_files_program.py
+++ src/fasta_files_program.py
@@ -15,9 +15,7 @@ Category
     DNA sequence
     
 Usage
-    El programa nos permite generar archivos con formato fasta a partir de un primer archivo 
-    que contiene las secuencias, el archivo dado puede sustituirse por otro siempre y cuando 
-    cuenten con el mismo formato.
+    src/fasta_files_program.py
     
 Arguments
     None
@@ -28,34 +26,31 @@ See also
 '''
 # Abrir el archivo en donde se encuentran las secuencias.
 # Guardar este contenido en una lista.
-# Darle el formato deseado
-# Insertar el caracter '>' y un salto de linea en la posicion 2
-# La secuencia de ADN queda en la posicion 3 de la lista, si esta tiene algun guion "-", susituirlo por cadena vacia ''
-# Convertir las minusculas a mayusculas
+# Generar una lista que contendra las secuencias sin  guiones y en mayusculas 
+# Las secuencias se encuentran a partir de la posicion 5 del string 
 
 # Agregar try y except para notificar al usuario si el archivo no existe:
 
 try:
     with open('data/dna_sequences.txt', 'r') as archivo:
-        secuencias = [line.split('\t') for line in archivo]
+        secuencias = [line for line in archivo]
 
 except IOError as io_error: 
     print(f"El archivo {io_error.filename} no se encuentra. \n")
     
 else:
-
-    for secuencia in secuencias:
-        secuencia.insert(0, '>')
-        secuencia.insert(2, '\n')
-        secuencia[3] = secuencia[3].replace('-', '').upper()
-
+    
+    format_seq = [secuencia[5:].upper().replace('-','') for secuencia in secuencias]
+    
 # Generar el archivo output
-# Escribir el contenido de la lista "secuencia" en el archivo
+# Escribir el encabezado de cada lista "> seq_" y con un contador generar los numeros.
+# Escribir el contenido de la lista "format_seq" en el archivo
 # cerrar el archivo
 
-    my_file = open('data/secuencias.fasta', 'w')
-    for secuencia in secuencias:
-        my_file.write(' '.join(secuencia))
+    my_file = open('results/secuencias.fasta', 'w')
+    
+    for i in range(0,len(format_seq)):
+        my_file.write(" > seq_" + str(i+1) + "\n" + format_seq[i])
     my_file.close()
 
 
