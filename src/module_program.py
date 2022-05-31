67c38355035613c7bffb1ace20d4f39b289b0114
Brenda <brenda.titanium@hotmail.com>
2022-05-31 16:17:46 -0500

    Actualizando modulos

---
+++ src/module_program.py
@@ -0,0 +1,30 @@
+''' 
+Name 
+    module_program 
+    
+Version
+    1.0
+    
+Author 
+    Lopez A. Brenda E.
+    
+Descripcion
+    Programa que llama a funciones de los modulos: ADN module y aminoacid modile
+    
+Category
+    Aminoacid and ADN
+    
+Usage
+    Python src/module_program 
+    
+Arguments
+
+    
+See also
+    None
+'''
+
+import ADN_module
+import aminoacids_module
+
+
