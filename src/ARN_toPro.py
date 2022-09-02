60ee38e25bada81b9d1fc2039851b3f6dd462cdb
Brenda <brenda.titanium@hotmail.com>
2022-09-02 18:28:05 -0500

    añadiendo archivos

--- src/ARN_toPro.py
+++ src/ARN_toPro.py
@@ -35,11 +35,15 @@ arg_parser = argparse.ArgumentParser(description="Translating ARN to protein")
 arg_parser.add_argument("-f", "--FILE",
                     metavar="path/to/file",
                     help="Archivo con secuencia de ARN",
-                    required=True)
+                    required=False)
+                    
+arg_parser.add_argument("-s", "--SEQUENCE",
+                    help="Secuencia de ARN",
+                    type=str,
+                    required=False)
                     
 arg_parser.add_argument("-o", "--OUTPUT",
                     help="Archivo de salida",
-                    type=int,
                     required=False)
 
 arg_parser.add_argument("-p","--PRINT",
@@ -49,27 +53,41 @@ arg_parser.add_argument("-p","--PRINT",
 args = arg_parser.parse_args()
 
 # Abrimos el archivo y extraemos su contenido
-with open(args.FILE, "r") as seq_file:
-    ARN = seq_file.read().upper()
-    
-gencode = {
-    'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M', 'ACA':'T',
-    'ACC':'T', 'ACG':'T', 'ACT':'T', 'AAC':'N', 'AAT':'N',
-    'AAA':'K', 'AAG':'K', 'AGC':'S', 'AGT':'S', 'AGA':'R',
-    'AGG':'R', 'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
-    'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P', 'CAC':'H',
-    'CAT':'H', 'CAA':'Q', 'CAG':'Q', 'CGA':'R', 'CGC':'R',
-    'CGG':'R', 'CGT':'R', 'GTA':'V', 'GTC':'V', 'GTG':'V',
-    'GTT':'V', 'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
-    'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E', 'GGA':'G',
-    'GGC':'G', 'GGG':'G', 'GGT':'G', 'TCA':'S', 'TCC':'S',
-    'TCG':'S', 'TCT':'S', 'TTC':'F', 'TTT':'F', 'TTA':'L',
-    'TTG':'L', 'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
-    'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W'}
+if args.FILE:
+    with open(args.FILE, "r") as seq_file:
+        ARN = seq_file.read().upper()
 
-def codon_format(ARN):    
-    codon_seq = [ARN[i:i+3] for i in range(0,len(ARN),3)]
-    return(codon_seq)
+if args.SEQUENCE:
+    ARN = args.SEQUENCE
+
+def arn_to_peptid(ARN):
+    gencode = {
+    'AUA':'I', 'AUC':'I', 'AUU':'I', 'AUG':'M', 'ACA':'T',
+    'ACC':'T', 'ACG':'T', 'ACU':'T', 'AAC':'N', 'AAU':'N',
+    'AAA':'K', 'AAG':'K', 'AGC':'S', 'AGU':'S', 'AGA':'R',
+    'AGG':'R', 'CUA':'L', 'CUC':'L', 'CUG':'L', 'CUU':'L',
+    'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCU':'P', 'CAC':'H',
+    'CAU':'H', 'CAA':'Q', 'CAG':'Q', 'CGA':'R', 'CGC':'R',
+    'CGG':'R', 'CGU':'R', 'GUA':'V', 'GUC':'V', 'GUG':'V',
+    'GUU':'V', 'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCU':'A',
+    'GAC':'D', 'GAU':'D', 'GAA':'E', 'GAG':'E', 'GGA':'G',
+    'GGC':'G', 'GGG':'G', 'GGU':'G', 'UCA':'S', 'UCC':'S',
+    'UCG':'S', 'UCU':'S', 'UUC':'F', 'UUU':'F', 'UUA':'L',
+    'UUG':'L', 'UAC':'Y', 'UAU':'Y', 'UAA':'_', 'UAG':'_',
+    'UGC':'C', 'UGU':'C', 'UGA':'_', 'UGG':'W'}
+ 
+    codon_sequence = [ARN[i:i+3] for i in range(0,len(ARN),3)]
+    peptid = [gencode.get(codon, "*") for codon in codon_sequence]
+    return(peptid)
 
 if evaluate_rna(ARN):
-    print()
+    peptid = arn_to_peptid(ARN)
+    if args.OUTPUT:
+        output_file = open(args.OUTPUT,'w')
+        output_file.write("".join(peptid))
+
+
+
+
+
+
