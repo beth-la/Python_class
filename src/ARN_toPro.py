cebfe8602da3cfc5b032d175a74dc0c7359e578e
Brenda <brenda.titanium@hotmail.com>
2022-09-02 17:45:33 -0500

    añadiendo argumentos

--- src/ARN_toPro.py
+++ src/ARN_toPro.py
@@ -24,7 +24,34 @@ Arguments
 See also
     None
 '''
+# Importamos librerias
+import argparse
+from posixpath import split
+import re 
+from aminoacids_module import evaluate
 
+# Paso de argumentos mediante argparse
+arg_parser = argparse.ArgumentParser(description="Translating ARN to protein")
+arg_parser.add_argument("-f", "--FILE",
+                    metavar="path/to/file",
+                    help="Archivo con secuencia de ARN",
+                    required=True)
+                    
+arg_parser.add_argument("-o", "--OUTPUT",
+                    help="Archivo de salida",
+                    type=int,
+                    required=False)
+
+arg_parser.add_argument("-p","--PRINT",
+                    help = "Imprimir a pantalla",
+                    required= False)
+
+args = arg_parser.parse_args()
+
+# Abrimos el archivo y extraemos su contenido
+with open(args.FILE, "r") as seq_file:
+    ARN = seq_file.read().upper()
+    
 gencode = {
     'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M', 'ACA':'T',
     'ACC':'T', 'ACG':'T', 'ACT':'T', 'AAC':'N', 'AAT':'N',
@@ -39,5 +66,10 @@ gencode = {
     'TCG':'S', 'TCT':'S', 'TTC':'F', 'TTT':'F', 'TTA':'L',
     'TTG':'L', 'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
     'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W'}
-    
-ARN = 'AACCTTGGGGG'
+
+def codon_format(ARN):    
+    codon_seq = [ARN[i:i+3] for i in range(0,len(ARN),3)]
+    return(codon_seq)
+
+if evaluate(ARN):
+    print()
